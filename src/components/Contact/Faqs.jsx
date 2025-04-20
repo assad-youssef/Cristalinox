d6288e936f92d1d1822abfc7026e1936ae6a82ac
assad-youssef <amine150assad@gmail.com>
2025-04-20 22:38:27 +0100

    seo FAQS

--- src/components/Contact/Faqs.jsx
+++ src/components/Contact/Faqs.jsx
@@ -1,49 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import "./Faqs.css";
+import { Helmet } from "react-helmet";
+
 const faqs = [
-    {
-      question: "Qu'est-ce que Cristal Inox ?",
-      answer:
-        "Cristal Inox est une entreprise spécialisée dans la chaudronnerie inoxydable. Nous proposons des services de fabrication et de transformation d'inox pour divers secteurs comme la décoration, le médical et le tuyautage industriel. Notre mission est d'offrir des solutions sur mesure avec une qualité irréprochable, adaptées aux besoins spécifiques de nos clients."
-    },
-    {
-      question: "Quels types de produits fabriquez-vous ?",
-      answer:
-        "Nous fabriquons une large gamme de produits en inox, allant des équipements industriels aux éléments de décoration. Cela inclut des garde-corps, des plans de travail, des cuves, des structures métalliques et bien plus encore. Chaque pièce est conçue avec précision et souci du détail pour garantir une finition impeccable."
-    },
-    {
-      question: "Pourquoi choisir l'inox pour vos projets ?",
-      answer:
-        "L'inox est un matériau exceptionnel grâce à sa résistance à la corrosion, sa durabilité et son esthétique moderne. Il est idéal pour des applications dans des environnements exigeants comme l'industrie alimentaire, la médecine ou l'architecture. Chez Cristal Inox, nous utilisons de l'inox de haute qualité pour assurer la longévité et la fiabilité de nos réalisations."
-    },
-    {
-      question: "Proposez-vous des services sur mesure ?",
-      answer:
-        "Oui, nous sommes spécialisés dans la fabrication sur mesure. Nous travaillons en étroite collaboration avec nos clients pour concevoir et réaliser des pièces uniques qui répondent parfaitement à leurs besoins. Que ce soit pour un projet architectural, industriel ou domestique, nous nous adaptons à vos exigences avec professionnalisme et expertise."
-    },
-    {
-      question: "Comment demander un devis ?",
-      answer:
-        "Vous pouvez nous contacter directement via notre site web, par téléphone ou par e-mail. Nous étudierons votre demande avec attention et vous proposerons une solution adaptée à votre budget et à vos exigences. Notre équipe est toujours disponible pour vous conseiller et vous accompagner dans la réalisation de votre projet."
-    },
-    {
-      question: "Quels sont vos délais de fabrication ?",
-      answer:
-        "Nos délais varient en fonction de la complexité du projet et de la charge de travail en cours. En général, un projet standard peut être réalisé en quelques semaines. Nous nous engageons à respecter des délais raisonnables tout en garantissant un travail de haute qualité. Pour obtenir une estimation précise, contactez-nous avec les détails de votre projet."
-    },
-    {
-      question: "Travaillez-vous avec des professionnels et des particuliers ?",
-      answer:
-        "Oui, nous collaborons aussi bien avec des entreprises qu'avec des particuliers. Que vous soyez un architecte, un industriel ou un particulier souhaitant un aménagement en inox sur mesure, nous avons l'expertise nécessaire pour répondre à vos attentes. Notre équipe est à votre écoute pour vous conseiller et concrétiser votre projet."
-    }
-  ];
+  {
+    question: "Qu'est-ce que Cristal Inox ?",
+    answer:
+      "Cristal Inox est une entreprise spécialisée dans la chaudronnerie inoxydable. Nous proposons des services de fabrication et de transformation d'inox pour divers secteurs comme la décoration, le médical et le tuyautage industriel. Notre mission est d'offrir des solutions sur mesure avec une qualité irréprochable, adaptées aux besoins spécifiques de nos clients.",
+  },
+  {
+    question: "Quels types de produits fabriquez-vous ?",
+    answer:
+      "Nous fabriquons une large gamme de produits en inox, allant des équipements industriels aux éléments de décoration. Cela inclut des garde-corps, des plans de travail, des cuves, des structures métalliques et bien plus encore. Chaque pièce est conçue avec précision et souci du détail pour garantir une finition impeccable.",
+  },
+  {
+    question: "Pourquoi choisir l'inox pour vos projets ?",
+    answer:
+      "L'inox est un matériau exceptionnel grâce à sa résistance à la corrosion, sa durabilité et son esthétique moderne. Il est idéal pour des applications dans des environnements exigeants comme l'industrie alimentaire, la médecine ou l'architecture. Chez Cristal Inox, nous utilisons de l'inox de haute qualité pour assurer la longévité et la fiabilité de nos réalisations.",
+  },
+  {
+    question: "Proposez-vous des services sur mesure ?",
+    answer:
+      "Oui, nous sommes spécialisés dans la fabrication sur mesure. Nous travaillons en étroite collaboration avec nos clients pour concevoir et réaliser des pièces uniques qui répondent parfaitement à leurs besoins. Que ce soit pour un projet architectural, industriel ou domestique, nous nous adaptons à vos exigences avec professionnalisme et expertise.",
+  },
+  {
+    question: "Comment demander un devis ?",
+    answer:
+      "Vous pouvez nous contacter directement via notre site web, par téléphone ou par e-mail. Nous étudierons votre demande avec attention et vous proposerons une solution adaptée à votre budget et à vos exigences. Notre équipe est toujours disponible pour vous conseiller et vous accompagner dans la réalisation de votre projet.",
+  },
+  {
+    question: "Quels sont vos délais de fabrication ?",
+    answer:
+      "Nos délais varient en fonction de la complexité du projet et de la charge de travail en cours. En général, un projet standard peut être réalisé en quelques semaines. Nous nous engageons à respecter des délais raisonnables tout en garantissant un travail de haute qualité. Pour obtenir une estimation précise, contactez-nous avec les détails de votre projet.",
+  },
+  {
+    question: "Travaillez-vous avec des professionnels et des particuliers ?",
+    answer:
+      "Oui, nous collaborons aussi bien avec des entreprises qu'avec des particuliers. Que vous soyez un architecte, un industriel ou un particulier souhaitant un aménagement en inox sur mesure, nous avons l'expertise nécessaire pour répondre à vos attentes. Notre équipe est à votre écoute pour vous conseiller et concrétiser votre projet.",
+  },{
+    question: "Dans quelles régions intervenez-vous ?",
+    answer:
+      "Nous sommes basés à Casablanca, mais nous intervenons dans tout le Maroc selon les projets. N'hésitez pas à nous contacter pour en savoir plus."
+  }
+  
+];
 const FAQ = () => {
   const [openIndex, setOpenIndex] = useState(null);
-   useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
-  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const toggleFAQ = (index) => {
     setOpenIndex(openIndex === index ? null : index);
@@ -51,13 +57,29 @@ const FAQ = () => {
 
   return (
     <div className="faq-container">
+      <Helmet>
+        <title>Contactez Cristal Inox | Adresse, téléphone, email</title>
+        <meta
+          name="description"
+          content="Besoin d'un expert en inox ? Contactez Cristal Inox à Casablanca. Téléphone, email, adresse et formulaire de contact disponibles ici."
+        />
+        <meta
+          name="keywords"
+          content="contact Cristal Inox, inox Casablanca, chaudronnerie inox, téléphone inox, email inox, adresse inox"
+        />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://www.cristalinox.ma/contact" />
+      </Helmet>
+
       <h1 className="faq-title">Foire Aux Questions - Cristal Inox💡</h1>
       <div className="faq-list">
         {faqs.map((faq, index) => (
           <div key={index} className="faq-item">
             <button className="faq-question" onClick={() => toggleFAQ(index)}>
               {faq.question}
-              <span className="faq-icon">{openIndex === index ? "➖" : "➕"}</span>
+              <span className="faq-icon">
+                {openIndex === index ? "➖" : "➕"}
+              </span>
             </button>
             {openIndex === index && (
               <motion.p
